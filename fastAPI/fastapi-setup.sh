94f8a09f3a32e502dca159d2c010ec2d712ab820
Alexandre Delaguillaumie <alexandre.delaguillaumie@gmail.com>
2023-02-11 19:42:42 +0100

    Changement des valeurs de fastapi-setup.sh + Réinitialisation des requirements

--- fastAPI/fastapi-setup.sh
+++ fastAPI/fastapi-setup.sh
@@ -1,11 +1,11 @@
 # Update and install requirements
 sudo apt-get update
-sudo apt install -y python3-pip nginx
+sudo apt install -y python3.10-pip nginx
 sudo apt install -y uvicorn
 # Copy the configuration file to the nginx enabled sites folder
 sudo cp -R fastapi_setup /etc/nginx/sites-enabled/
 sudo service nginx restart
-cd ~/AWS-EC2-Setup-for-FastAPI-and-Ngrok-Deployment
+cd ~/ Implementez-un-modele-de-scoring/fastAPI/
 # Install FastAPI application requirements
 pip3 install -r requirements.txt
 # Kill any service running on port 80
