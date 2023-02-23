8885e35f4b38fda5174c9f79d894c507e0b3dba9
Alexandre Delaguillaumie <alexandre.delaguillaumie@gmail.com>
2023-02-23 16:08:18 +0100

    Version finale de ce qui va composer ce dépot git

--- README.md
+++ README.md
@@ -1,1 +1,74 @@
-# Implementez-un-modele-de-scoring
+<h1>Projet 7: Implémentez un modèle de scoring</h1>
+
+<h2>Contexte et problématique du projet</h2>
+
+Vous êtes Data Scientist au sein d'une société financière, nommée "Prêt à dépenser",  qui propose des crédits à la consommation pour des personnes ayant peu ou pas du tout d'historique de prêt.
+
+L’entreprise souhaite mettre en œuvre un outil de “scoring crédit” pour calculer la probabilité qu’un client rembourse son crédit, puis classifie la demande en crédit accordé ou refusé. Elle souhaite donc développer un algorithme de classification en s’appuyant sur des sources de données variées (données comportementales, données provenant d'autres institutions financières, etc.).
+
+De plus, les chargés de relation client ont fait remonter le fait que les clients sont de plus en plus demandeurs de transparence vis-à-vis des décisions d’octroi de crédit. Cette demande de transparence des clients va tout à fait dans le sens des valeurs que l’entreprise veut incarner.
+
+Prêt à dépenser décide donc de développer un dashboard interactif pour que les chargés de relation client puissent à la fois expliquer de façon la plus transparente possible les décisions d’octroi de crédit, mais également permettre à leurs clients de disposer de leurs informations personnelles et de les explorer facilement. 
+
+<h2>But et intérêt du projet</h2>
+
+Construire un modèle de scoring qui donnera une prédiction sur la probabilité de faillite d'un client de façon automatique. Données disponible ici : https://www.kaggle.com/c/home-credit-default-risk/data
+
+- Possibilité de s'appuyer sur un kernel Kaggle pour faciliter l’analyse exploratoire, la préparation des données et le feature engineering nécessaires à l’élaboration du modèle de scoring
+- Utiliser des pipelines d'entrainement
+- Élaborer et choisir parmi plusieurs modèle de classification celui qui aura les meilleurs résultats avec une technique d'optimisation des hyper-paramètres
+- Formaliser des mesures et résultats de chaque expérimentation, afin de les analyser et de les comparer sur ML Flow
+- Mettre en place une démarche de type MLOps d’automatisation et d’industrialisation de la gestion du cycle de vie du modèle. Liste des outils ici : https://s3.eu-west-1.amazonaws.com/course.oc-static.com/projects/Data_Scientist_P7/Outils+Open+Source+MLOps.pdf
+- Créer une métrique personnalisée sous la forme d'une fonction coût répondant aux problématiques métier
+- Utiliser un seuil de classification pour l'octroi d'un crédit
+- Analyser la feature importance locale et globale avec SHAP
+- Détecter éventuellement du Data Drift sur les principales features, entre les datas d’entraînement et les datas de production, au travers d'un tableau HTML d’analyse avec la librairie Evidently
+- Rédiger une note méthodologique décrivant la méthodologie d'entraînement du modèle, le traitement du déséquilibre des classes, la fonction coût métier, l'algorithme d'optimisation et la métrique d'évaluation , un tableau de synthèse des résultats, l’interprétabilité globale et locale du modèle,les limites et les améliorations possibles, l’analyse du Data Drift
+
+
+Construire un dashboard interactif à destination des gestionnaires de la relation client permettant d'interpréter les prédictions faites par le modèle, et d’améliorer la connaissance client des chargés de relation client.
+
+- Visualiser le score et l’interprétation de ce score pour chaque client de façon intelligible pour une personne non experte en data science.
+- Visualiser des informations descriptives relatives à un client (via un système de filtre).
+- Comparer les informations descriptives relatives à un client à l’ensemble des clients ou à un groupe de clients similaires.
+
+Mettre en production le modèle de scoring de prédiction à l’aide d’une API, ainsi que le dashboard interactif qui appelle l’API pour les prédictions.
+
+- Utiliser un logiciel de version de code pour assurer l’intégration du modèle
+
+<h2>Compétences évaluées</h2>
+
+- Réaliser un dashboard pour présenter son travail de modélisation
+- Présenter son travail de modélisation à l'oral
+- Évaluer les performances des modèles d’apprentissage supervisé selon différents critères
+- Utiliser un logiciel de version de code pour assurer l’intégration du modèle
+- Définir et mettre en œuvre un pipeline d’entraînement des modèles
+- Définir la stratégie d’élaboration d’un modèle d’apprentissage supervisé
+- Déployer un modèle via une API dans le Web
+- Définir et mettre en œuvre une stratégie de suivi de la performance d’un modèle
+- Rédiger une note méthodologique afin de communiquer sa démarche de modélisation
+
+<h2>Contenu du dépôt GitHub</h2>
+
+- README.md: fichier de présentation du projet
+
+- Répertoire "fastapi":
+  - fichier "main.py": code de l'API. peut retourner une prediction ou des shap_values d'un client
+  - fichier "model.pkl": modèle de classification exporté via mlflow.sklearn.log_model
+  - fichier "Procfile": script pour déployer le code sur Heroku
+  - fichier "README.md": Explique le fonctionnement de l'API
+  - fichier "requirements.txt": Contient toutes les librairies et dépendances nécéssaires pour faire tourner le code
+  - fichier "sample_test_set.pickle": dataset sous format DataFrame de la librairie pandas
+
+- Répertoire "Streamlit":
+  - fichier "Dashboard.py":
+  - fichier "infos_client.pickle":
+  - fichier "model.pkl": modèle de classification exporté via mlflow.sklearn.log_model
+  - fichier "preprocessed_data.pickle": model pré-entrainé pour récupérer le nom des colonnes après traitement
+  - fichier "pret_client.pickle":
+  - fichier "Procfile": script pour déployer le code sur Heroku
+  - fichier "README.md": Explique le fonctionnement de l'API
+  - fichier "requirements.txt": Contient toutes les librairies et dépendances nécéssaires pour faire tourner le code
+  - fichier "runtime.txt" : donne la bonne version de Python compatible avec Heroku et les librairies
+  - fichier "sample_test_set.pickle": dataset sous format DataFrame de la librairie pandas
+  - fichier "setup.sh": script de configuration de l'interface Streamlit
--- Streamlit/Dashboard.py
+++ Streamlit/Dashboard.py
@@ -1,92 +1,173 @@
+#-----------------------#
+# IMPORT DES LIBRAIRIES #
+#-----------------------#
+
 import streamlit as st
-import pickle
+import joblib
 import plotly.graph_objects as go
-import matplotlib.pyplot as plt
-import matplotlib.lines as mlines
-import seaborn as sns
+import matplotlib as plt
+import plotly.express as px
+st.set_option('deprecation.showPyplotGlobalUse', False)
 import shap
-import json
 import requests as re
-import datetime
 import numpy as np
-import pandas as pd
-from PIL import Image
-from pathlib import Path
-import os
 
-# Dashboard interactif permettant :
-#  - aux chargés de relation client d'expliquer de façon la plus transparente possible les décisions d’octroi de crédit
-#  - à leurs clients de disposer de leurs informations personnelles et de les explorer facilement.
 
-# Celui-ci contient les fonctionnalités suivantes :
-#  - Permettre de visualiser le score et l’interprétation de ce score pour chaque client de façon intelligible pour
-#    une personne non experte en data science.
-#  - Permettre de visualiser des informations descriptives relatives à un client (via un système de filtre).
-#  - Permettre de comparer les informations descriptives relatives à un client à l’ensemble des clients ou à un groupe
-#    de clients similaires.
+#---------------------#
+# VARIABLES STATIQUES #
+#---------------------#
 
-# st.set_page_config(
-#     page_title="Loan Prediction App",
-#     page_icon="./loan.png"
-# )
+#API_PRED = "https://api-creditscore.herokuapp.com/predict/"
+#API_SHAP = "https://api-creditscore.herokuapp.com/shap_client/"
+API_PRED = "http://127.0.0.1:8000/predict/"
+API_SHAP = "http://127.0.0.1:8000/shap_client/"
 
-PREDICTION_API_URL = "http://localhost:5000"
-# PREDICTION_API_URL = "http://192.168.1.71:5000"
+data = joblib.load('sample_test_set.pickle')
+infos_client = joblib.load('infos_client.pickle')
+pret_client = joblib.load('pret_client.pickle')
+preprocessed_data = joblib.load('preprocessed_data.pickle')
+model = joblib.load('model.pkl')
 
-DATEORIGIN = datetime.datetime(2018, 5, 17)  # Date de la compétition Kaggle
-N_CRIT = 15
-N_CAT_MAX = 10
+column_names = preprocessed_data.columns.tolist()
+expected_value = -2.9159221699244515
+threshold = 100-10.344827586206896
 
-IMG_PATH = Path(__file__).parent
-print(IMG_PATH)
+classifier = model.named_steps['classifier']
+df_preprocess = model.named_steps['preprocessor'].transform(data)
+explainer = shap.TreeExplainer(classifier)
+generic_shap = explainer.shap_values(df_preprocess, check_additivity=False)
 
-# Suppression des marges par défaut
-padding = 1
-st.markdown(f""" <style>
-    .reportview-container .main .block-container{{
-        padding-top: {padding}rem;
-        padding-right: {padding}rem;
-        padding-left: {padding}rem;
-        padding-bottom: {padding}rem;
-    }} </style> """, unsafe_allow_html=True)
+html="""           
+    <h1 style="font-size:400%; color:DarkSlateGrey; font-family:Soleil"> DASHBOARD <br>
+        <body style="font-size:100%, color:DarkSlateGrey, font-family:Sofia Pro"> <br>
+        </body>
+     </h1>
+"""
+st.markdown(html, unsafe_allow_html=True)
 
-st.title("Prêt à dépenser - tableau de bord")
-st.markdown("""---""")
-st.write(
-    "Application web prédisant le risque de défaut de paiement d'un client. L'application utilise un modèle de Machine Learning, servi par une API.")
-st.markdown("""---""")
+#---------#
+# SIDEBAR #
+#---------#
 
-# Récupération des paramètres d'affichage
+#Profile Client
+profile_ID = st.sidebar.selectbox('Sélectionnez un client :',
+                                  list(data.index))
+API_GET = API_PRED+(str(profile_ID))
+score_client = 100-int(re.get(API_GET).json()*100)
+if score_client < threshold:
+    st.sidebar.write("Prêt refusé")
+else:
+    st.sidebar.write("Prêt accordé.")
 
-# Panneau latéral
-st.sidebar.title("Informations client")
-st.session_state.client_container = st.sidebar
+# Affichage de la jauge
+jauge = go.Figure(go.Indicator(
+                  mode='gauge+number+delta',
+                  value=score_client,
+                  domain={'x': [0, 1], 'y': [0, 1]},
+                  gauge={'axis': {'range': [None,100],
+                                  'tickwidth': 3,
+                                  'tickcolor': 'DarkSlateGrey'},
+                         'bar': {'color': '#F0F2F6', 'thickness': 1},
+                         'steps': [{'range': [0,50], 'color': 'Crimson'},
+                                   {'range': [50,70], 'color': 'Orange'},
+                                   {'range': [70,100-10.344827586206896], 'color': 'red'},
+                                   {'range': [100-10.344827586206896,95], 'color': 'LimeGreen'},
+                                   {'range': [95,100], 'color': 'Green'}],
+                         'threshold': {'line': {'color': 'DarkSlateGrey', 'width': 2},
+                                       'thickness': 1,
+                                       'value' : threshold}}))
 
-st.sidebar.button("Référence client")
+jauge.update_layout(height=250, width=305,
+                    font={'color': 'black', 'family': 'Sofia Pro'},
+                    margin=dict(l=0, r=0, b=0, t=0, pad=2))
 
-# Chargement des données
-data = pd.read_csv('dataset_final.csv',
-                   index_col=0)
-data.to_pickle('data.pkl')
-data = pd.read_pickle('data.pkl')
+st.sidebar.plotly_chart(jauge)
+if 95 <= score_client < 100:
+    score_text = 'DOSSIER PARFAIT'
+    st.sidebar.success(score_text)
+elif threshold <= score_client < 95:
+    score_text = 'DOSSIER CORRECT'
+    st.sidebar.success(score_text)
+elif 70 <= score_client < threshold:
+    score_text = 'DOSSIER À RÉVISER'
+    st.sidebar.warning(score_text)
+else :
+    score_text = 'DOSSIER INSOLVABLE'
+    st.sidebar.error(score_text)
 
-# CHOIX DU CLIENT
+#---------------------------------------#
+# INFORMATIONS GÉNÉRIQUES SUR LE CLIENT #
+#---------------------------------------#
+
+# Infos principales client
+html="""
+     <h3 style="font-size:150%; color:DarkSlateGrey; font-family:Sofia Pro"> Récapitulatif du profil<br>
+     </h3>
+"""
+st.markdown(html, unsafe_allow_html=True)
+client_info = infos_client[infos_client.index == profile_ID].iloc[:, :]
+st.table(client_info)
+
+# Infos principales sur la demande de prêt
+html="""
+     <h3 style="font-size:150%; color:DarkSlateGrey; font-family:Sofia Pro"> Caractéristiques du prêt<br>
+     </h3>
+"""
+st.markdown(html, unsafe_allow_html=True)
+client_pret = pret_client[pret_client.index== profile_ID].iloc[:, :]
+st.table(client_pret)
+
+
+#-----------------------#
+# CRÉATION DE VARIABLES #
+#-----------------------#
+
+html = """
+         <h3 style="font-size:150%; color:DarkSlateGrey; font-family:Sofia Pro"> Contribution des variables au modèle <br>
+         </h3>
+    """
+st.markdown(html, unsafe_allow_html=True)
 
 with st.container():
-    col1, col2 = st.columns([1, 3])
+    col1,col2 = st.columns([4,3.3])
     with col1:
-        st.write("")
-        col1.header("**ID Client**")
-        client_id = col1.selectbox('Sélectionnez un client :',
-                                   data['SK_ID_CURR'].unique())
+        # Interprétabilité locale SHAP
+        st.write("Pour le client {}:".format(profile_ID))
+        API_GET = API_SHAP + (str(profile_ID))
+        shap_values = re.get(API_GET).json()
+        shap_values = np.array(shap_values[1:-1].split(',')).astype('float32')
+        waterfall = shap.plots._waterfall.waterfall_legacy(expected_value=expected_value,
+                                                           shap_values=shap_values,
+                                                           feature_names=column_names,
+                                                           max_display=20)
+        st.pyplot(waterfall)
     with col2:
-        # Infos principales client
-        # st.write("*Traits stricts*")
-        client_info = data[data['SK_ID_CURR'] == client_id].iloc[:, :]
-        client_info.set_index('SK_ID_CURR', inplace=True)
-        st.table(client_info)
-        # Infos principales sur la demande de prêt
-        # st.write("*Demande de prêt*")
-        client_pret = data[data['SK_ID_CURR'] == client_id].iloc[:, :]
-        client_pret.set_index('SK_ID_CURR', inplace=True)
-        st.table(client_pret)
+        # Interprétabilité globale SHAP
+        st.write("Pour l'ensemble des clients:")
+        # shap.plots.bar(dict(shap_values), max_display=40)
+        summary = shap.summary_plot(shap_values=generic_shap,
+                                    feature_names=column_names,
+                                    max_display=20)
+        st.pyplot(waterfall)
+
+# Graphique interactifs
+html = """
+             <h3 style="font-size:150%; color:DarkSlateGrey; font-family:Sofia Pro"> Classification graphique interactive <br>
+             </h3>
+        """
+st.markdown(html, unsafe_allow_html=True)
+features = st.multiselect("Choisissez deux variables",
+                          list(data.columns),
+                          default=['AMT_ANNUITY','AMT_INCOME_TOTAL'],
+                          max_selections=2)
+if len(features) != 2:
+    st.error("Sélectionnez deux variables")
+else:
+    # Graphique
+    chart = px.scatter(data,
+                       x=features[0],
+                       y=features[1],
+                       color='TARGET',
+                       color_discrete_sequence=['LimeGreen','Tomato'],
+                       hover_name=data.index)
+    st.plotly_chart(chart)
